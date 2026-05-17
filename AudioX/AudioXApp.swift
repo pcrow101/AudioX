edcfd61e1e2dd3d6208a50a250e92ee8573c3bde
pcrow101 <34235799+pcrow101@users.noreply.github.com>
2026-05-17 16:16:02 +0100

    Initial commit

---
+++ AudioX/AudioXApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioXApp.swift
+//  AudioX
+//
+//  Created by paul crow on 17/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct AudioXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioXTests/AudioXTests.swift
@@ -0,0 +1,19 @@
+//
+//  AudioXTests.swift
+//  AudioXTests
+//
+//  Created by paul crow on 17/05/2026.
+//
+
+import Testing
+@testable import AudioX
+
+struct AudioXTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
